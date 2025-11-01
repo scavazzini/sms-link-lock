b565436f0e8623108a9b2eb7e157872f3a41d92d
Newton Scavazzini <newtonscavazzini@gmail.com>
2025-11-01 12:50:45 -0300

    create chat balloon components

---
+++ app/src/main/java/dev/scavazzini/smslinklock/feature/chat/component/InfoChatBalloon.kt
@@ -0,0 +1,111 @@
+package dev.scavazzini.smslinklock.feature.chat.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+data class InfoChatBalloonOption(
+    val text: String,
+    val onClick: () -> Unit,
+)
+
+@Composable
+internal fun InfoChatBalloon(
+    text: String,
+    modifier: Modifier = Modifier,
+    options: List<InfoChatBalloonOption> = emptyList(),
+) {
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .widthIn(max = 320.dp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .background(
+                    color = Color.DarkGray,
+                    shape = RoundedCornerShape(8.dp),
+                )
+                .padding(8.dp),
+        ) {
+            Text(
+                text = text,
+                color = Color.White,
+                fontStyle = FontStyle.Italic,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.padding(8.dp),
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                modifier = Modifier.background(Color.DarkGray),
+            ) {
+                options.forEach { option ->
+                    Button(
+                        onClick = option.onClick,
+                        modifier = Modifier.padding(vertical = 0.dp),
+                        colors = ButtonDefaults.buttonColors().copy(
+                            containerColor = Color.White,
+                            contentColor = Color.DarkGray,
+                        ),
+                    ) {
+                        Text(option.text)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun InfoChatBalloonPreview() {
+    InfoChatBalloon(text = "Connection established")
+}
+
+@Preview
+@Composable
+private fun InfoChatBalloonWithOptionsPreview() {
+    InfoChatBalloon(
+        text = "Alice wants to start a secure chat with you",
+        options = listOf(
+            InfoChatBalloonOption("Reject", {}),
+            InfoChatBalloonOption("Accept", {}),
+        ),
+    )
+}
+
+
+@Preview()
+@Composable
+private fun InfoChatBalloonLongTextPreview() {
+    InfoChatBalloon(
+        text = "Duis vel dolor tincidunt, dignissim turpis sed, semper metus. Praesent aliquam fringilla aliquet. Nullam consectetur lectus ut ipsum accumsan, et blandit augue posuere.",
+        options = listOf(
+            InfoChatBalloonOption("Huh?", {}),
+            InfoChatBalloonOption("Understood", {}),
+        ),
+    )
+}
---
+++ app/src/main/java/dev/scavazzini/smslinklock/feature/chat/component/TextChatBalloon.kt
@@ -0,0 +1,182 @@
+package dev.scavazzini.smslinklock.feature.chat.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.LockOpen
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@Composable
+internal fun TextChatBalloon(
+    text: String,
+    sentByYou: Boolean,
+    signed: Boolean,
+    datetime: LocalDateTime,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = if (sentByYou) Alignment.End else Alignment.Start,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            if (sentByYou) {
+                BalloonInfo(signed)
+            }
+
+            Balloon(text, sentByYou)
+
+            if (!sentByYou) {
+                BalloonInfo(signed)
+            }
+        }
+
+        Text(text = datetime.format())
+    }
+}
+
+@Composable
+fun BalloonInfo(signed: Boolean, modifier: Modifier = Modifier) {
+    Icon(
+        modifier = modifier
+            .size(24.dp)
+            .clickable(onClick = {}),
+        imageVector = if (signed) Icons.Default.Lock else Icons.Default.LockOpen,
+        contentDescription = null,
+    )
+}
+
+@Composable
+private fun Balloon(
+    text: String,
+    sentByYou: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    val colors = defaultColors(sentByYou)
+    val shape = defaultShape(sentByYou)
+
+    Column(
+        modifier = modifier
+            .background(
+                color = colors.backgroundColor,
+                shape = shape,
+            )
+            .border(width = 1.dp, color = colors.borderColor, shape = shape)
+            .padding(16.dp),
+    ) {
+        SelectionContainer(
+            Modifier.widthIn(min = 10.dp, max = 280.dp)
+        ) {
+            Text(
+                text = text,
+                color = Color.Black,
+                modifier = Modifier.wrapContentWidth()
+            )
+        }
+    }
+}
+
+private data class TextChatBalloonColors(
+    val backgroundColor: Color,
+    val borderColor: Color,
+)
+
+private fun defaultColors(sentByYou: Boolean): TextChatBalloonColors {
+    if (sentByYou) {
+        return TextChatBalloonColors(
+            backgroundColor = Color(0xFF90D5FF),
+            borderColor = Color(0xFF77B1D4),
+        )
+    }
+    return TextChatBalloonColors(
+        backgroundColor = Color(0xFFFFFFC5),
+        borderColor = Color(0xFFB3B389),
+    )
+}
+
+private fun defaultShape(sentByYou: Boolean): RoundedCornerShape {
+    val largerRadius = 32.dp
+    val smallerRadius = 0.dp
+
+    if (sentByYou) {
+        return RoundedCornerShape(
+            topStart = largerRadius,
+            topEnd = largerRadius,
+            bottomEnd = smallerRadius,
+            bottomStart = largerRadius,
+        )
+    }
+    return RoundedCornerShape(
+        topStart = largerRadius,
+        topEnd = largerRadius,
+        bottomEnd = largerRadius,
+        bottomStart = smallerRadius,
+    )
+}
+
+private fun LocalDateTime.format(): String {
+    val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+    val fullDateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")
+
+    if (LocalDateTime.now().minusDays(1).isBefore(this)) {
+        return timeFormatter.format(this)
+    }
+
+    return fullDateTimeFormatter.format(this)
+}
+
+@Preview
+@Composable
+private fun TextChatBalloonShortMessagePreview() {
+    TextChatBalloon(
+        text = "Short message",
+        sentByYou = true,
+        signed = true,
+        datetime = LocalDateTime.now().minusMinutes(50),
+    )
+}
+
+@Preview
+@Composable
+private fun TextChatBalloonLongMessagePreview() {
+    TextChatBalloon(
+        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget ullamcorper massa. Morbi elementum dui et viverra malesuada. Aliquam mollis interdum ante, ut tempus arcu lacinia sed. Aenean neque eros, condimentum ac porta non, rhoncus eu dolor.\n\nSuspendisse pellentesque lorem at nunc suscipit gravida. Integer malesuada, ipsum non pharetra pellentesque, purus tortor placerat leo, eu blandit eros neque sit amet ex. Aliquam porttitor auctor vestibulum. Nunc ut malesuada est. Donec iaculis sem sem, non imperdiet lectus semper consectetur. Etiam vel congue dolor. Aliquam bibendum quam sed fermentum efficitur. Nullam vitae nulla lorem.\n\nDonec interdum porttitor arcu, et euismod lectus mattis sed.",
+        sentByYou = false,
+        signed = true,
+        datetime = LocalDateTime.now().minusDays(3),
+    )
+}
+
+@Preview
+@Composable
+private fun TextChatBalloonUnsignedPreview() {
+    TextChatBalloon(
+        text = "Unsigned message",
+        sentByYou = false,
+        signed = false,
+        datetime = LocalDateTime.now().minusYears(3),
+    )
+}
